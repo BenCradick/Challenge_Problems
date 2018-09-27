f262e446fb8558058005cce2372c02ade4701733
Ben Cradick <ben.cradick@gmail.com>
2018-09-27 09:29:29 -0500

    not fast enough, all fib numbers - 1 are 1 off fix causes prgram to be too slow at large numbers

--- challenge2.py
+++ challenge2.py
@@ -1,10 +1,27 @@
 import math
 
-PHI = (1 + 5 ** 0.5) / 2
+PHI = (1 + math.sqrt(5)) / 2
 SQ5 = math.sqrt(5)
-def fibonacci(n):
 
-    return int(math.floor(math.log(n*SQ5 + 0.5,PHI)) - 1)
+
+
+def fibonacci(n):
+    if is_fib(n + 1):
+        return math.floor(math.log(n*SQ5 + 1/PHI, PHI) - 1)
+    else:
+        return math.floor(math.log(n*SQ5 + 1/PHI, PHI) - 2)
+
+def is_fib(n):
+    k = float(5*n**2)
+    j = (k - 4)
+    l = (k + 4)
+    j1 = math.floor(math.sqrt(j))**2
+    l1 = math.floor(math.sqrt(l))**2
+    if (j == j1) or (l == l1):
+        return True
+    else:
+        return False
+
 
 def exponential_series(n):
     height = math.log(n + 1,2)
@@ -12,6 +29,3 @@ def exponential_series(n):
 
 def answer(total_lambs):
     return fibonacci(total_lambs) - exponential_series(total_lambs)
-
-# for n in range(1, 99):
-#     print(n, fibonacci(n), exponential_series(n), answer(n))
